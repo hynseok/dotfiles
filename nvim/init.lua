c79a43dfb371002fb44d00e821f58e59cec7400f
hynseok <wefwef12e@naver.com>
2026-04-03 17:48:05 +0900

    fix nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,8 @@ require("config.setting")
 vim.lsp.enable('luals')
 vim.lsp.enable('clangd')
 vim.lsp.enable('pylsp')
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
