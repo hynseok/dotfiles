8c4f81dd474a42926843f2725bb6ab9fe2d3496c
hynseok <wefwef12e@naver.com>
2026-04-03 17:45:37 +0900

    fix install script

--- install.sh
+++ install.sh
@@ -1,31 +1,69 @@
 #!/bin/bash
 # thanks larsks, https://stackoverflow.com/questions/75146772/how-to-detect-os-system-using-bash
 
-set -e
+# set -e
 
 DOTFILES_DIR="$HOME/dotfiles"
+CURRENT_SHELL=$(basename "$SHELL")
+
+echo "Installing NeoVim"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm ./nvim-linux-x86_64.tar.gz
+
+if [ "$CURRENT_SHELL" = "zsh" ]; then
+    TARGET_FILE="$HOME/.zshrc"
+elif [ "$CURRENT_SHELL" = "bash" ]; then
+    TARGET_FILE="$HOME/.bashrc"
+else
+    echo "Your shell is not supported: $CURRENT_SHELL"
+    exit 1
+fi
+
+read -r -d '' CONTENT << 'EOF'
+# from dotfiles
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias c='clear'
+alias vi='nvim'
+alias vim='nvim'
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+EOF
+
+echo "$CONTENT" | while IFS= read -r line || [ -n "$line" ]; do
+    if ! grep -Fxq "$line" "$TARGET_FILE"; then
+        echo "$line" >> "$TARGET_FILE"
+        echo "Added: $line"
+    else
+        echo "Skipped (already exists): $line"
+    fi
+done
+
+echo "Configuration update complete for $TARGET_FILE"
+
+source $TARGET_FILE
 
 . /etc/os-release
 
 case $ID in
   ubuntu) echo "Detected distribution : Ubuntu"
-	echo "Installing Dependencies.."
+	echo "Installing Tmux.."
 	sudo apt update
-	echo "this dotfiles need nvim version newer than 0.11 but with apt you can't get it."
-	echo "Try install it by yourself."
-	#sudo apt update && sudo apt install -y neovim
 	sudo apt install -y tmux
-	source $DOTFILES_DIR/nvim/install.sh
-	source $DOTFILES_DIR/tmux/install.sh
     ;;
 
   arch) echo "Detected distribution : Arch"
-	echo "Installing Dependencies.."
-	yes | sudo pacman -Syu && yes | sudo pacman -S nvim tmux
-	source $DOTFILES_DIR/nvim/install.sh
-	source $DOTFILES_DIR/tmux/install.sh
+	echo "Installing Tmux.."
+	yes | sudo pacman -Syu && yes | sudo pacman -S tmux
     ;;
 
   *) echo "Your distribution is not supported :("
-      ;;
+     exit 1  
+    ;;
 esac
+
+source $DOTFILES_DIR/nvim/install.sh
+source $DOTFILES_DIR/tmux/install.sh
+
--- nvim/install-lsp.sh
+++ nvim/install-lsp.sh
@@ -1,9 +1,20 @@
 . /etc/os-release
 
 echo "Installing LSPs.."
+
+TARGET_LINK="$HOME/.local/bin/lua-language-server"
+
 case $ID in
   ubuntu)
-	sudo apt update && sudo apt install -y clangd; sudo apt install -y lua-language-server; sudo apt install -y python3-pylsp
+	sudo apt update && sudo apt install -y clangd; sudo apt install -y python3-pylsp
+    	if [ ! -e "$TARGET_LINK" ]; then
+            mkdir -p ~/.local/share/lua-lang-server && \
+            tar --extract --file lua-language-server-3.17.1-linux-x64.tar.gz -av -C ~/.local/share/lua-lang-server && \
+            ln -s ~/.local/share/lua-lang-server/bin/lua-language-server "$TARGET_LINK" && \
+            rm ./lua-language-server-3.17.1-linux-x64.tar.gz
+        else
+            echo "skip: lua-language-server already exists"
+fi
     ;;
 
   arch) 
